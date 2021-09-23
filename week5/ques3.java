ac83b044f8b6d3dd967e9b74f5583f4a214918db
shubhangidourby <76735411+shubhangidourby@users.noreply.github.com>
2021-09-23 15:34:56 +0530

    update week5

--- week5/ques3.java
+++ week5/ques3.java
@@ -3,27 +3,22 @@ package week5;
 import java.util.Scanner;
 
 public class ques3 {
-	static int printUnion(int arr1[], int arr2[], int m, int n)
-	{
-		int i = 0, j = 0;
-		while (i < m && j < n) {
-			if (arr1[i] < arr2[j])
-				System.out.print(arr1[i++] + " ");
-			else if (arr2[j] < arr1[i])
-				System.out.print(arr2[j++] + " ");
-			else {
-				System.out.print(arr2[j++] + " ");
-				i++;
-			}
-		}
-		while (i < m)
-			System.out.print(arr1[i++] + " ");
-		while (j < n)
-			System.out.print(arr2[j++] + " ");
-
-		return 0;
+    static void printIntersection(int arr1[], int arr2[], int m, int n)
+    {
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (arr1[i] < arr2[j])
+                i++;
+            else if (arr2[j] < arr1[i])
+                j++;
+            else {
+                System.out.print(arr2[j++] + " ");
+                i++;
+            }
+        }
     }
-	public static void main(String args[])
+  
+		public static void main(String[] args)
 	{
 		Scanner sc= new Scanner(System.in);
         int t= sc.nextInt();
@@ -41,7 +36,7 @@ public class ques3 {
         {
             arr2[k]=sc.nextInt();
         }
-		printUnion(arr1, arr2, m, n);
+		printIntersection(arr1, arr2, m, n);
         }
 	}
 }
